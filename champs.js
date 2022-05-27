969b85213847948ceee863be6be811c6234447e4
skero323 <gaj.skrjanc@gmail.com>
2022-05-27 12:12:48 +0200

    without Bel'Veth

---
+++ champs.js
@@ -0,0 +1,619 @@
+
+const champs = [
+    {
+        "id": "aatrox",
+        "rune": "Grasp - Boneplating",
+    },
+    {
+        "id": "ahri",
+        "rune": "First strike - treasure hunter",
+    },
+    {
+        "id": "akali",
+        "rune": "First strike - treasure hunter",
+    },
+    {
+        "id": "alistar",
+    },
+    {
+        "id": "askhan",
+        "rune": "First strike - resolve",
+    },
+    {
+        "id": "amumu",
+    },
+    {
+        "id": "anivia",
+        "rune": "First strike - dematirializer - treasure hunter",
+    },
+    {
+        "id": "annie",
+        "rune": "First strike - treasure hunter",
+    },
+    {
+        "id": "aphelios",
+        "rune": "First strike - treasure hunter",
+    },
+    {
+        "id": "ashe",
+    },
+    {
+        "id": "sol",
+        "rune": "First strike - dematirializer - treasure hunter",
+    },
+    {
+        "id": "azir",
+        "rune": "First strike - dematirializer - treasure hunter",
+    },
+    {
+        "id": "bard",
+    },
+    {
+        "id": "blitzcrank",
+    },
+    {
+        "id": "brand",
+        "rune": "First strike - dematirializer - treasure hunter",
+    },
+    {
+        "id": "braum",
+    },
+    {
+        "id": "caitlyn",
+    },
+    {
+        "id": "camille",
+        "rune": "Grasp - second wind",
+    },
+    {
+        "id": "cass",
+        "rune": "First strike - treasure hunter",
+    },
+    {
+        "id": "chogath",
+        "rune": "First strike - resolve",
+    },
+    {
+        "id": "corki",
+        "rune": "First strike - dematirializer - treasure hunter",
+    },
+    {
+        "id": "darius",
+        "rune": "First strike - resolve OR Grasp - boneplating",
+    },
+    {
+        "id": "diana",
+        "rune": "First strike - treasure hunter",
+    },
+    {
+        "id": "draven",
+        "rune": "First strike - dematirializer - treasure hunter",
+    },
+    {
+        "id": "mundo",
+        "rune": "First strike - treasure hunter",
+    },
+    {
+        "id": "ekko",
+        "rune": "First strike - treasure hunter",
+    },
+    {
+        "id": "elise",
+    },
+    {
+        "id": "evelynn",
+    },
+    {
+        "id": "ezreal",
+    },
+    {
+        "id": "fiddlesticks",
+    },
+    {
+        "id": "fiora",
+        "rune": "Grasp - second wind",
+    },
+    {
+        "id": "fizz",
+        "rune": "First strike - resolve",
+    },
+    {
+        "id": "galio",
+        "rune": "First strike - dematirializer - treasure hunter",
+    },
+    {
+        "id": "gangplank",
+        "rune": "Grasp - second wind",
+    },
+    {
+        "id": "garen",
+        "rune": "First strike - resolve",
+    },
+    {
+        "id": "gnar",
+        "rune": "First strike - treasure hunter",
+    },
+    {
+        "id": "gragas",
+        "rune": "First strike - dematirializer - treasure hunter",
+    },
+    {
+        "id": "graves",
+        "rune": "Grasp - second wind",
+    },
+    {
+        "id": "gwen",
+        "rune": "First strike - resolve",
+    },
+    {
+        "id": "hecarim",
+        "rune": "Grasp - second wind",
+    },
+    {
+        "id": "heimer",
+        "rune": "First strike - dematirializer - treasure hunter",
+    },
+    {
+        "id": "illaoi",
+        "rune": "First strike - treasure hunter",
+    },
+    {
+        "id": "irelia",
+        "rune": "First strike - resolve",
+    },
+    {
+        "id": "ivern",
+        "rune": "First strike - dematirializer - treasure hunter",
+    },
+    {
+        "id": "janna",
+        "rune": "First strike - dematirializer - treasure hunter",
+    },
+    {
+        "id": "jarvan",
+        "rune": "Grasp - second wind",
+    },
+    {
+        "id": "jax",
+        "rune": "First strike - resolve",
+    },
+    {
+        "id": "jayce",
+        "rune": "Grasp - second wind",
+    },
+    {
+        "id": "jhin",
+    },
+    {
+        "id": "jinx",
+    },
+    {
+        "id": "kaisa",
+        "rune": "First strike - dematirializer - treasure hunter",
+    },
+    {
+        "id": "kalista",
+        "rune": "First strike - dematirializer - treasure hunter",
+    },
+    {
+        "id": "karma",
+        "rune": "First strike - treasure hunter",
+    },
+    {
+        "id": "karthus",
+        "rune": "First strike - dematirializer - treasure hunter",
+    },
+    {
+        "id": "kassadin",
+        "rune": "First strike - treasure hunter",
+    },
+    {
+        "id": "katarina",
+        "rune": "First strike - dematirializer - treasure hunter",
+    },
+    {
+        "id": "kayle",
+        "rune": "First strike - treasure hunter",
+    },
+    {
+        "id": "kayn",
+        "rune": "First strike - resolve",
+    },
+    {
+        "id": "kennen",
+        "rune": "First strike - treasure hunter",
+    },
+    {
+        "id": "khazix",
+        "rune": "First strike - dematirializer - treasure hunter",
+    },
+    {
+        "id": "kindred",
+    },
+    {
+        "id": "kled",
+        "rune": "Grasp - Boneplating",
+    },
+    {
+        "id": "kogmaw",
+        "rune": "First strike - dematirializer - treasure hunter",
+    },
+    {
+        "id": "leblanc",
+        "rune": "First strike - treasure hunter",
+    },
+    {
+        "id": "leesin",
+        "rune": "Grasp - second wind",
+    },
+    {
+        "id": "leona",
+    },
+    {
+        "id": "lillia",
+        "rune": "First strike - treasure hunter",
+    },
+    {
+        "id": "liss",
+        "rune": "First strike - treasure hunter",
+    },
+    {
+        "id": "lucian",
+        "rune": "Grasp - second wind",
+    },
+    {
+        "id": "lulu",
+        "rune": "Grasp - second wind",
+    },
+    {
+        "id": "lux",
+        "rune": "First strike - dematirializer - treasure hunter",
+    },
+    {
+        "id": "malphite",
+        "rune": "Grasp - second wind",
+    },
+    {
+        "id": "malzahar",
+        "rune": "First strike - dematirializer - treasure hunter",
+    },
+    {
+        "id": "maokai",
+        "rune": "First strike - treasure hunter",
+    },
+    {
+        "id": "yi",
+        "rune": "First strike - resolve",
+    },
+    {
+        "id": "missfortune",
+    },
+    {
+        "id": "monkeyking",
+    },
+    {
+        "id": "morde",
+        "rune": "First strike - dematirializer - treasure hunter",
+    },
+    {
+        "id": "morgana",
+        "rune": "First strike - dematirializer - treasure hunter",
+    },
+    {
+        "id": "nami",
+    },
+    {
+        "id": "nasus",
+    },
+    {
+        "id": "nautilus",
+        "rune": "First strike - treasure hunter",
+    },
+    {
+        "id": "neeko",
+        "rune": "First strike - treasure hunter",
+    },
+    {
+        "id": "nidalee",
+        "rune": "First strike - treasure hunter",
+    },
+    {
+        "id": "nocturne",
+        "rune": "Grasp - Boneplating",
+    },
+    {
+        "id": "nunu",
+        "rune": "First strike - dematirializer - treasure hunter",
+    },
+    {
+        "id": "olaf",
+        "rune": "First strike - treasure hunter",
+    },
+    {
+        "id": "ori",
+        "rune": "First strike - dematirializer - treasure hunter",
+    },
+    {
+        "id": "ornn",
+        "rune": "First strike - treasure hunter",
+    },
+    {
+        "id": "pantheon",
+        "rune": "First strike - resolve",
+    },
+    {
+        "id": "poppy",
+        "rune": "First strike - treasure hunter",
+    },
+    {
+        "id": "pyke",
+        "rune": "First strike - dematirializer - treasure hunter",
+    },
+    {
+        "id": "qiyana",
+        "rune": "First strike - treasure hunter",
+    },
+    {
+        "id": "quinn",
+        "rune": "Grasp - second wind",
+    },
+    {
+        "id": "rakan",
+        "rune": "First strike - dematirializer - treasure hunter",
+    },
+    {
+        "id": "rammus",
+        "rune": "First strike - dematirializer - treasure hunter",
+    },
+    {
+        "id": "reksai",
+    },
+    {
+        "id": "renekton",
+        "rune": "Grasp - second wind",
+    },
+    {
+        "id": "rengar",
+        "rune": "Grasp - Boneplating",
+    },
+    {
+        "id": "riven",
+        "rune": "First strike - resolve",
+    },
+    {
+        "id": "rumble",
+        "rune": "Grasp - second wind",
+    },
+    {
+        "id": "ryze",
+        "rune": "First strike - treasure hunter",
+    },
+    {
+        "id": "samira",
+        "rune": "First strike - treasure hunter",
+    },
+    {
+        "id": "sejuani",
+        "rune": "First strike - dematirializer - treasure hunter",
+    },
+    {
+        "id": "senna",
+    },
+    {
+        "id": "seraphine",
+        "rune": "First strike - dematirializer - treasure hunter",
+    },
+    {
+        "id": "sett",
+        "rune": "First strike - resolve",
+    },
+    {
+        "id": "shaco",
+        "rune": "First strike - treasure hunter",
+    },
+    {
+        "id": "shen",
+        "rune": "First strike - treasure hunter",
+    },
+    {
+        "id": "shyvana",
+        "rune": "First strike - treasure hunter",
+    },
+    {
+        "id": "singed",
+        "rune": "First strike - dematirializer - treasure hunter",
+    },
+    {
+        "id": "sion",
+        "rune": "First strike - treasure hunter",
+    },
+    {
+        "id": "sivir",
+    },
+    {
+        "id": "skarner",
+        "rune": "First strike - dematirializer - treasure hunter",
+    },
+    {
+        "id": "sona",
+    },
+    {
+        "id": "soraka",
+        "rune": "First strike - dematirializer - treasure hunter",
+    },
+    {
+        "id": "swain",
+        "rune": "First strike - treasure hunter",
+    },
+    {
+        "id": "sylas",
+        "rune": "First strike - resolve",
+    },
+    {
+        "id": "syndra",
+        "rune": "First strike - dematirializer - treasure hunter",
+    },
+    {
+        "id": "tahm",
+        "rune": "First strike - treasure hunter",
+    },
+    {
+        "id": "taliyah",
+        "rune": "First strike - treasure hunter",
+    },
+    {
+        "id": "talon",
+        "rune": "First strike - treasure hunter",
+    },
+    {
+        "id": "taric",
+        "rune": "First strike - dematirializer - treasure hunter",
+    },
+    {
+        "id": "teemo",
+        "rune": "Grasp - second wind",
+    },
+    {
+        "id": "thresh",
+        "rune": "First strike - treasure hunter",
+    },
+    {
+        "id": "tristana",
+        "rune": "First strike - resolve",
+    },
+    {
+        "id": "trundle",
+        "rune": "First strike - resolve",
+    },
+    {
+        "id": "tryndamere",
+        "rune": "Grasp - Boneplating",
+    },
+    {
+        "id": "tf",
+        "rune": "First strike - dematirializer - treasure hunter",
+    },
+    {
+        "id": "twitch",
+    },
+    {
+        "id": "udyr",
+        "rune": "First strike - resolve",
+    },
+    {
+        "id": "urgot",
+        "rune": "Grasp - second wind",
+    },
+    {
+        "id": "varus",
+        "rune": "First strike - dematirializer - treasure hunter",
+    },
+    {
+        "id": "vayne",
+        "rune": "Grasp - second wind",
+    },
+    {
+        "id": "veigar",
+        "rune": "First strike - dematirializer - treasure hunter",
+    },
+    {
+        "id": "velkoz",
+        "rune": "First strike - dematirializer - treasure hunter",
+    },
+    {
+        "id": "vi",
+        "rune": "First strike - resolve",
+    },
+    {
+        "id": "viktor",
+        "rune": "First strike - dematirializer - treasure hunter",
+    },
+    {
+        "id": "vladimir",
+        "rune": "First strike - treasure hunter",
+    },
+    {
+        "id": "volibear",
+        "rune": "First strike - resolve",
+    },
+    {
+        "id": "vex",
+        "rune": "First strike - dematirializer - treasure hunter",
+    },
+    {
+        "id": "viego",
+        "rune": "First strike - resolve",
+    },
+    {
+        "id": "warwick",
+        "rune": "First strike - resolve",
+    },
+    {
+        "id": "wukong",
+        "rune": "First strike - resolve",
+    },
+    {
+        "id": "xayah",
+    },
+    {
+        "id": "xerath",
+        "rune": "First strike - dematirializer - treasure hunter",
+    },
+    {
+        "id": "xin",
+        "rune": "First strike - resolve",
+    },
+    {
+        "id": "yasuo",
+        "rune": "First strike - treasure hunter",
+    },
+    {
+        "id": "yone",
+        "rune": "First strike - treasure hunter",
+    },
+    {
+        "id": "yorick",
+        "rune": "First strike - resolve",
+    },
+    {
+        "id": "yuumi",
+    },
+    {
+        "id": "zac",
+        "rune": "First strike - dematirializer - treasure hunter",
+    },
+    {
+        "id": "zed",
+        "rune": "First strike - resolve",
+    },
+    {
+        "id": "ziggs",
+        "rune": "First strike - dematirializer - treasure hunter",
+    },
+    {
+        "id": "zilean",
+        "rune": "First strike - dematirializer - treasure hunter",
+    },
+    {
+        "id": "zoe",
+        "rune": "First strike - treasure hunter",
+    },
+    {
+        "id": "zyra",
+    }
+]
+//document.getElementById("izpis").innerHTML = champs[0]["id"];
+let form = document.getElementById("form")
+form.addEventListener("submit", event => {
+    event.preventDefault();
+    let ime = document.getElementById("ime").value;
+    let rune = document.getElementById("izpis");
+    //let hp = document.getElementById("hp");
+    //let armor = document.getElementById("armor");
+    for (let i = 0; i < champs.length; i++) {
+        if (champs[i]["id"] == ime) {
+            rune.innerHTML = champs[i]["rune"];
+            //hp.innerHTML = "Health "+champs[i]["stats"]["hp"];
+        }
+    }
+})
